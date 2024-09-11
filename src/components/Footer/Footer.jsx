4db0766bdfd5077fc5e43cdf80f3f262c24fe38c
Deepak Gupta <dkguptadln98@gmail.com>
2024-09-11 11:30:17 +0530

    Remove unwanted script

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,15 +3,13 @@ import MiddleFooter from './MiddleFooter/MiddleFooter'
 import PrimaryFooter from './PrimaryFooter/PrimaryFooter'
 import CopyRight from './CopyRight/CopyRight'
 
-import "../../assets/js/vendor/jquary-3.6.0.min.js"
+import "../../assets/js/vendor/jquery-3.6.0.min.js"
 import "../../assets/js/vendor/bootstrap-bundle.js"
-import "../../assets/js/vendor/imagesloaded-pkgd.js"
 import "../../assets/js/vendor/waypoints.min.js"
 import "../../assets/js/vendor/venobox.min.js"
 import "../../assets/js/vendor/odometer.min.js"
 import "../../assets/js/vendor/meanmenu.js"
 import "../../assets/js/vendor/smooth-scroll.js"
-import "../../assets/js/vendor/jquery.isotope.js"
 import "../../assets/js/vendor/countdown.js"
 import "../../assets/js/vendor/swiper.min.js"
 import "../../assets/js/vendor/nice-select.js"
